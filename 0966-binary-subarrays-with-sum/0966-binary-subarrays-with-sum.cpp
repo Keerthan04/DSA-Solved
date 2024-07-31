b6b5640a0f6264257473c0965fd183b889dd9651
-_- <117875736+Keerthan04@users.noreply.github.com>
2024-07-31 12:22:29 +0530

    Time: 29 ms (42.16%), Space: 31.5 MB (47.74%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -1,15 +1,29 @@
 class Solution {
 public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        unordered_map<int,int> map;
-        int prefix=0;int cnt=0;
-        map[0]=1;//initialize 0 to 1
-        for(int i=0;i<nums.size();i++){
-            prefix+=nums[i];
-            int rem = prefix-goal;//find reminder to check if it is in map
-            cnt+=map[rem];//count of the prefix ka we shd add
-            map[prefix]+=1;//add all
+    int lessequaltok(vector<int>& nums,int goal){
+        if(goal < 0)
+        return 0;//since minus of function the goal - 1 we are doing os if goal is 0 then to avoid that we are doing this
+
+        int l = 0;
+        int r = 0;
+        int ans = 0;
+        int n = nums.size();
+        int sum = 0;
+
+        while(r < n){
+        sum += nums[r];
+
+        while(sum > goal){
+            sum -= nums[l];
+            l++;
         }
-        return cnt;
+        ans += (r-l+1);
+        r++;
+        }
+        return ans;
+        
+    }
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return lessequaltok(nums,goal)-lessequaltok(nums,goal-1);
     }
 };
