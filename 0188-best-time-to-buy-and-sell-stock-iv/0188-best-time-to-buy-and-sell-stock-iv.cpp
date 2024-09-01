7de6ae51f3b5f9b26cbfcbab90d1eca8d49fb9fd
-_- <117875736+Keerthan04@users.noreply.github.com>
2024-09-01 18:34:03 +0530

    Time: 12 ms (50.08%), Space: 16.9 MB (38.78%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-    int solve(int ind,int buy,int cap,vector<int> &prices,vector<vector<vector<int>>>&dp){
-        if(cap==0) return 0;//cant get anything from the market
-        if(ind==prices.size()) return 0;//cant get anything from market
-        if(dp[ind][buy][cap]!=-1) return dp[ind][buy][cap];
-        if(buy){
-            //can buy so
-            dp[ind][buy][cap] = max(-prices[ind] + solve(ind+1,0,cap,prices,dp),solve(ind+1,1,cap,prices,dp));
-        }
-        else{
-            //shd sell
-            dp[ind][buy][cap] = max(prices[ind]+ solve(ind+1,1,cap-1,prices,dp),solve(ind+1,0,cap,prices,dp));
-        }
-        return dp[ind][buy][cap];
-    }
     int maxProfit(int k, vector<int>& prices) {
        int n=prices.size();
-        vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int>(k+1,-1)));//is a 3d dp
-        return solve(0,1,k,prices,dp); 
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(k+1,0)));//is a 3d dp
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                for (int cap = 1; cap <= k; cap++) {//cap 1 as 0 means 0 anyway
+                    if (buy == 0) { // We can buy the stock
+                        dp[ind][buy][cap] = max(0 + dp[ind + 1][0][cap],
+                                            -prices[ind] + dp[ind + 1][1][cap]);
+                    }
+
+                    if (buy == 1) { // We can sell the stock
+                        dp[ind][buy][cap] = max(0 + dp[ind + 1][1][cap],
+                                            prices[ind] + dp[ind + 1][0][cap - 1]);
+                    }
+                }
+            }
+        }
+
+        // The result is stored in dp[0][0][2] which represents maximum profit after the final transaction.
+        return dp[0][0][k]; 
     }
 };
