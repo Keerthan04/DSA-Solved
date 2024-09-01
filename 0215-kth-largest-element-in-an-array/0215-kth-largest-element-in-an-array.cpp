18c41da40b13d4e9e01e90415a08abe7202ddbb6
-_- <117875736+Keerthan04@users.noreply.github.com>
2024-09-01 20:26:31 +0530

    Time: 105 ms (31.47%), Space: 61.4 MB (48.78%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,30 +1,14 @@
 class Solution {
 public:
-    // Function to count numbers greater than or equal to mid
-    int countGreaterOrEqual(int mid, vector<int>& arr) {
-        int count = 0;
-        for (int num : arr) {
-            if (num >= mid) count++;
-        }
-        return count;
-    }
-
+    //since basic k and largest given we can use the heap and solve
     int findKthLargest(vector<int>& nums, int k) {
-        int start = *min_element(nums.begin(), nums.end());
-        int end = *max_element(nums.begin(), nums.end());
-
-        while (start <= end) {  // Change to allow correct search space convergence
-            int mid = start + (end - start) / 2;
-            int large_num = countGreaterOrEqual(mid, nums);
-
-            if (large_num >= k) {
-                start = mid + 1;  // Move start to mid + 1 if we have too many large elements
-            } else {
-                end = mid - 1;  // Move end to mid - 1 if we don't have enough large elements
+        priority_queue<int, vector<int>, greater<int>> minh;
+        for (int i = 0; i < nums.size(); i++) {
+            minh.push(nums[i]);
+            if (minh.size() > k) {
+                minh.pop();
             }
         }
-
-        // Return the element where the binary search stops
-        return end;  // 'end' will point to the correct k-th largest element
+        return minh.top();
     }
 };
