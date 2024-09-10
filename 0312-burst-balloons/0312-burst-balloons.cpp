d81dc96cfd3a25b4756fb45a4ce6806a3216e95b
-_- <117875736+Keerthan04@users.noreply.github.com>
2024-09-10 18:42:57 +0530

    Time: 99 ms (93.71%), Space: 12.4 MB (36.7%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,25 +1,5 @@
 class Solution {
 public:
-    // Recursive function to calculate the maximum coins obtained
-int maxCoinsHelper(int i, int j, vector<int> &nums, vector<vector<int>> &dp) {
-        if (i > j) return 0;
-        if (dp[i][j] != -1) return dp[i][j];
-        int maxCoins = INT_MIN;
-        
-        // Iterate through each possible balloon to burst last
-        for (int k = i; k <= j; k++) {
-            // Calculate the coins obtained by bursting the k-th balloon last
-            int coins = nums[i - 1] * nums[k] * nums[j + 1];
-            
-            // Recursively calculate the maximum coins for the remaining balloons
-            int remainingCoins = maxCoinsHelper(i, k - 1, nums, dp) + maxCoinsHelper(k + 1, j, nums, dp);
-            
-            // Update the maximum coins
-            maxCoins = max(maxCoins, coins + remainingCoins);
-        }
-        
-        return dp[i][j] = maxCoins;
-    }
     int maxCoins(vector<int>& nums) {
         int n = nums.size();
     
@@ -28,9 +8,29 @@ int maxCoinsHelper(int i, int j, vector<int> &nums, vector<vector<int>> &dp) {
         nums.push_back(1);
         
         // Create a DP array for memoization
-        vector<vector<int>> dp(n + 2, vector<int>(n + 2, -1));
+        vector<vector<int>> dp(n + 2, vector<int>(n + 2, 0));
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= n; j++) {
+                if (i > j) continue;
+                int maxi = INT_MIN;
+                
+                // Iterate through each possible balloon to burst last
+                for (int ind = i; ind <= j; ind++) {
+                    // Calculate the coins obtained by bursting the ind-th balloon last
+                    int coins = nums[i - 1] * nums[ind] * nums[j + 1];
+                    
+                    // Calculate the maximum coins for the remaining balloons
+                    int remainingCoins = dp[i][ind - 1] + dp[ind + 1][j];
+                    
+                    // Update the maximum coins
+                    maxi = max(maxi, coins + remainingCoins);
+                }
+                
+                dp[i][j] = maxi;
+            }
+        }
         
-        // Call the helper function to compute the maximum coins
-        return maxCoinsHelper(1, n, nums, dp);
+        return dp[1][n];
     }
 };
