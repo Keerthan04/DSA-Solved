fcdb427d4c5b7e436e13aff46dd2f3a0001ddf30
-_- <117875736+Keerthan04@users.noreply.github.com>
2024-12-22 12:02:37 +0530

    Time: 0 ms (100%), Space: 18.1 MB (52.23%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> indegree(numCourses, 0);
+        vector<vector<int>> adj(numCourses);
+        vector<int> topo;
+
+        // Build the graph and calculate indegree
+        for (auto& prerequisite : prerequisites) {
+            adj[prerequisite[1]].push_back(prerequisite[0]);
+            indegree[prerequisite[0]]++;
+        }
+
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) q.push(i);
+        }
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+
+            for (int neighbor : adj[node]) {
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0) q.push(neighbor);
+            }
+        }
+
+        // Check if topological sorting is possible
+        if (topo.size() == numCourses) return topo;
+        else return {};
+    }
+};
