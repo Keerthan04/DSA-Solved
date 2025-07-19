cdaffe942a1d996f253739f56fe88cbec999528c
-_- <117875736+Keerthan04@users.noreply.github.com>
2025-07-19 19:29:40 +0530

    Time: 4 ms (11.97%), Space: 16.8 MB (6.31%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
-    void solve(vector<vector<int>>& answers, vector<int> nums, vector<int> indianswers) {
-        if (nums.size() == 0) {
+    void solve(vector<vector<int>>& answers, vector<int> nums, vector<int> indianswers,int index) {
+        if (index == nums.size()) {
             answers.push_back(indianswers);
             return;
         }
-
-        // Option 1: Do not include the first element of nums
-        vector<int> op1 = indianswers;
-
-        // Option 2: Include the first element of nums
-        vector<int> op2 = indianswers;
-        op2.push_back(nums[0]);
-
-        // Remove the first element from nums for the recursive calls
-        nums.erase(nums.begin());
-
-        // Recurse with the two options
-        solve(answers, nums, op1);
-        solve(answers, nums, op2);
-
-        return;
+        //choose the ele in index
+        indianswers.push_back(nums[index]);
+        solve(answers,nums,indianswers,index+1);//then call from next
+        //now dont pick so remove the added ele in indianswers
+        indianswers.pop_back();
+        solve(answers,nums,indianswers,index+1);//again run fo next elements
     }
 
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> answers;
         vector<int> indianswers;
-        solve(answers, nums, indianswers);
+        solve(answers, nums, indianswers,0);
         return answers;
     }
 };
