fab22109dc47fa73260a5ab837bba5acfcffe0fc
-_- <117875736+Keerthan04@users.noreply.github.com>
2025-07-29 07:58:09 +0530

    Time: 14 ms (18.86%), Space: 11 MB (40.56%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        vector<int> count(26);
-        int maxCount = 0;
-    
-        int i = 0;
-        int j = 0;
-        
-        int result = 0;
-        
-        while (j < s.size()) {
-            count[s[j] - 'A']++;
-            maxCount = max(maxCount, count[s[j] - 'A']);
-            if (j - i + 1 - maxCount > k) {
-                count[s[i] - 'A']--;
-                i++;
+        int maxRepeatingChar = 0;
+        int maxLen = 0;
+        unordered_map<char,int> freq;
+        int wstart = 0;
+        for(int wend = 0;wend<s.length();wend++){
+            //so here we need to perform on the back of it ka
+            //in this case it is to add freq and find max one
+            freq[s[wend]]++;
+            //this is to tell us the which is the max repeat char so that keep that and we will replace the other ones
+            maxRepeatingChar = max(maxRepeatingChar,freq[s[wend]]);
+            //now is the constraint to reduce the window
+            if(wend-wstart+1 - maxRepeatingChar > k){
+                //so if the window length - most repeated char length > k then we cant replace with k swaps so shrink the window from start
+                freq[s[wstart]]--;
+                wstart++;
             }
-            result = max(result, j - i + 1);
-            j++;
+            //so now we have proper thing so maxlen calculate
+            maxLen = max(maxLen,wend-wstart+1);
         }
-        
-        return result;
+        return maxLen;
     }
 };
