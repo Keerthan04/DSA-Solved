4d2fe66f168974dc12b8ec32ef2f448509866d88
-_- <117875736+Keerthan04@users.noreply.github.com>
2025-07-31 20:01:06 +0530

    Time: 0 ms (100%), Space: 17.7 MB (17.34%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
+    int search(vector<int> &nums,int target,bool findingLast){
+        int keyindex = -1;
+        int start = 0; int end = nums.size()-1;
+        while(start<=end){
+            int mid = start + (end - start)/2;
+            if(target > nums[mid]) start = mid+1;
+            else if(target < nums[mid]) end = mid-1;
+            else{
+                //then = key it is
+                keyindex = mid;
+                if(findingLast){
+                    //finding last occurence then
+                    start = mid+1;
+                }else{
+                    //finding first so
+                    end = mid -1;
+                }
+            }
+        }
+        return keyindex;
+    }
     vector<int> searchRange(vector<int>& nums, int target) {
-        int start=0,end=nums.size()-1;
-        int firstOccur=-1,lastOccur=-1;
-        if(nums.size()==0) return {-1,-1};
-        //getting first occur
-        while(start<=end){
-            int mid = start + ((end-start)/2);
-            if(target == nums[mid]){
-                firstOccur=mid;
-                end=mid-1;
-            }
-            else if(target>nums[mid]){
-                start=mid+1;
-            }else{
-                end=mid-1;
-            }
-        }
-        start=0;
-        end=nums.size()-1;
-        while(start<=end){
-            int mid = start + ((end-start)/2);
-            if(target == nums[mid]){
-                lastOccur=mid;
-                start=mid+1;
-            }
-            else if(target>nums[mid]){
-                start=mid+1;
-            }else{
-                end=mid-1;
-            }
-        }
-        return {firstOccur,lastOccur};
-
+        vector<int> result(2, -1); // Initialize with two elements
+        result[0] = search(nums, target, false); // Find first occurrence
+        if (result[0] == -1) return result;      // Not found
+        result[1] = search(nums, target, true);  // Find last occurrence
+        return result;
     }
 };
